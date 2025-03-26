723a6ace39d6d06b0e58cf3eb73ea6f505eb8d6d
Martin Dahlén Jansson <martin@savelend.se>
2025-03-26 11:21:21 +0100

    Update cp.php

--- routes/cp.php
+++ routes/cp.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Savelend\PloiDeployer\Http\Controllers\PloiDeployController;
-use Savelend\PloiDeployer\Http\Controllers\PloiConfigController;
 
-Route::group(['middleware' => ['statamic.cp.authenticated']], function () {
-    Route::post('ploi-deployer/deploy', [PloiDeployController::class, 'deploy'])->name('ploi-deployer.deploy');
+Route::group(['middleware' => ['statamic.cp.authenticated'], 'namespace' => 'Savelend\PloiDeployer\Http\Controllers'], function() {
+    Route::post('ploi-deployer/deploy', 'PloiDeployController@deploy')->name('ploi-deployer.deploy');
 
-    Route::get('ploi-deployer/config', [PloiConfigController::class, 'index'])->name('ploi-deployer.config.index');
-    Route::post('ploi-deployer/config', [PloiConfigController::class, 'update'])->name('ploi-deployer.config.update');
+    Route::get('ploi-deployer/config', 'PloiConfigController@index')->name('ploi-deployer.config.index');
+    Route::post('ploi-deployer/config', 'PloiConfigController@update')->name('ploi-deployer.config.update');
 });
