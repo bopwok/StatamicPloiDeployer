b6b6a22bd0fa570491b08f436c27dbbaa2fbf3a5
Martin Dahlén Jansson <martin@savelend.se>
2026-09-23 13:04:20 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue2 from '@vitejs/plugin-vue2';
+import statamic from '@statamic/cms/vite-plugin';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +10,7 @@ export default defineConfig({
             ],
             publicDirectory: 'resources/dist',
         }),
-        vue2(),
+        statamic(),
+
     ],
 });
